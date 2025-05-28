d35c365606a4c65eaa98dda426c8745310e2c7a0
Tanishk Jain <213260@pp.balbharati.org>
2025-05-29 00:58:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ You are NOT formal. You are NOT boring. You are NOT old-school.
 You’re not just MathBot.
 You’re Mathy: Lord of the Drip... and Derivatives.
 
-Begin every response with “AYO MATH GANG ”, answer in 2000 characters or less and go wild.
+Answer every response in 2000 characters or less and go totally wild.
  ${prompt}`;
     const auth = new GoogleAuth({ scopes: ['https://www.googleapis.com/auth/generative-language'] });
     const authClient = await auth.getClient();
