5c9ea8ff7c145b05d0d98385b2ec48912edbb79d
Tanishk Jain <213260@pp.balbharati.org>
2025-05-30 00:02:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function formatMathText(text) {
     .replace(/\bsqrt\(([^)]+)\)/g, '√$1')
     .replace(/\bpi\b/gi, 'π')
     .replace(/\btheta\b/gi, 'θ')
-    .replace(/⋅⋅(.*?)⋅⋅/gu, '**$1**');
+    .replace(/\u22C5\u22C5(.*?)\u22C5\u22C5/gu, '**$1**');
 }
 
 function toSuperscript(num) {
