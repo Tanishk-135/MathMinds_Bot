943a397e14c810c7cf6359f368efeb3d7d055dd3
Tanishk Jain <213260@pp.balbharati.org>
2025-06-10 15:19:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 require('dotenv').config();
 const { storeMessage, getRecentMessagesWithContext } = require("./redisSetup.js");
 const { Client, GatewayIntentBits, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent, // Required for reading message content
+    ],
+});
 const { GoogleAuth } = require('google-auth-library');
 const { exec } = require('child_process');
 const util = require('util');
